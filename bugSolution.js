d81fb9658f0566d13b6057d03dc5cb7877d3d654
BugHunter-X <askzenai@gmail.com>
2024-12-31 01:22:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,27 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const [isMounted, setIsMounted] = useState(true);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => {
+      setIsMounted(false);
+      console.log('Component unmounting');
+    };
+  }, []);
+
+  useEffect(() => {
+    if(isMounted) {
+      console.log('Count updated:', count);
+    }
+  }, [count, isMounted]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
